06dee1173c0f18a16a799f7c51a553cb6a242720
Asep <asep@autentika.id>
2021-08-04 14:26:07 +0700

    add test suite

---
+++ Test Suites/test suite 1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f634c8b4-379c-4fc8-b69d-9ada455923f3</testSuiteGuid>
+</TestSuiteEntity>
